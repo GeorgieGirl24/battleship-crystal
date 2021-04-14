2c202c7a25c5f6996f0126c6dff9d2f6a5711971
George Soderholm <georgesoderholm@gmail.com>
2021-04-13 21:25:51 -0600

    Add methods #place_submarine and #place_cruiser

--- lib/computer.cr
+++ lib/computer.cr
@@ -10,4 +10,32 @@ class Computer
     @submarine = Ship.new("Submarine", 2)
     @ship_count = 2
   end
+
+  def place_submarine
+    loop do
+      submarine_coordinates = Array(String).new
+      until submarine_coordinates.size == submarine.length
+        submarine_coordinates << board.@cells.keys.sample
+      end
+      if board.valid_placement?(@submarine, submarine_coordinates)
+        board.place(@submarine, submarine_coordinates)
+        return submarine_coordinates
+        break
+      end
+    end
+  end
+
+  def place_cruiser
+    loop do
+      cruiser_coordinates = Array(String).new
+      until cruiser_coordinates.size == cruiser.length
+        cruiser_coordinates << board.@cells.keys.sample
+      end
+      if board.valid_placement?(@cruiser, cruiser_coordinates)
+        board.place(@cruiser, cruiser_coordinates)
+        return cruiser_coordinates
+        break
+      end
+    end
+  end
 end
