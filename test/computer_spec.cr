499b108458866f6072c70e3cb012fc06a234eba7
George Soderholm <georgesoderholm@gmail.com>
2021-04-13 20:37:01 -0600

    Add tests to see if the board can place a ship

--- test/computer_spec.cr
+++ test/computer_spec.cr
@@ -21,7 +21,40 @@ describe Computer do
     end
   end
 
-  describe "target" do
+  describe "a ship can be placed on the board " do
+    it "can place a cruiser"do
+      board = Board.new
+      computer = Computer.new
+      cruiser = Ship.new("Cruiser", 3)
+      submarine = Ship.new("Submarine", 2)
+      cruiser_coordinates = ["D2", "D3", "D4"]
+      submarine_coordinates = ["B2", "C2"]
+
+      board.valid_placement?(cruiser, cruiser_coordinates).should eq true
+      board.valid_placement?(cruiser, cruiser_coordinates).should_not eq false
+
+      board.place(cruiser, cruiser_coordinates).should eq [cruiser, cruiser, cruiser]
+      board.place(cruiser, cruiser_coordinates).should_not eq [cruiser, cruiser, cruiser, cruiser]
+      board.place(cruiser, cruiser_coordinates).should_not eq [cruiser, cruiser]
+      board.place(cruiser, cruiser_coordinates).should_not eq [submarine, submarine, submarine]
+    end
+
+    it "can place a submarine"do
+    board = Board.new
+    computer = Computer.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    cruiser_coordinates = ["D2", "D3", "D4"]
+    submarine_coordinates = ["B2", "C2"]
+
+    board.valid_placement?(submarine, submarine_coordinates).should eq true
+    board.valid_placement?(submarine, submarine_coordinates).should_not eq false
+
+    board.place(submarine, submarine_coordinates).should eq [submarine, submarine]
+    board.place(submarine, submarine_coordinates).should_not eq [submarine, submarine, submarine, submarine]
+    board.place(submarine, submarine_coordinates).should_not eq [submarine, submarine, submarine]
+    board.place(submarine, submarine_coordinates).should_not eq [cruiser, cruiser]
+    end
 
   end
 end
